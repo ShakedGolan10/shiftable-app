6cd892d26c92bcfe868be4d9d9ecdcb2c7447486
Shaked Golan <shaked518@gmail.com>
2024-06-02 13:30:03 +0300

    removed comment

--- services/exports.tsx
+++ services/exports.tsx
@@ -1,14 +1,6 @@
 import dynamic from "next/dynamic";
 import Loading from "@/components/loading-element";
 
-// Map must be dynamically loaded, since leaflet updates the DOM on import, breaking server-side rendering
-// export const EmployeeHomePage = dynamic(() => import("../components/employee-home-page"), {
-// 	ssr: false,
-// 	loading: function LoadingComponent() {
-// 		return <Loading />;
-// 	},
-// });
-
 export const ShiftsApplicationLayout = dynamic(() => import("../layouts/shift_application_layout"), {
 	ssr: false,
 	loading: function LoadingComponent() {
