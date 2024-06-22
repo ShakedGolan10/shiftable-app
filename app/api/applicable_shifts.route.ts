4e94fdeab73bba25b8595185f364eddb1be1fbeb
Shaked Golan <shaked518@gmail.com>
2024-06-22 17:02:33 +0300

    troubleshooting deploy error - deleting file causing error to check !

--- app/api/applicable_shifts.route.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-
-export default async function GET(req: NextRequest, res: NextResponse) {
-    console.log('arrived')
-    // const userId = request.nextUrl.searchParams.get('userId')
-
-}
--- services/server-services/shifts.service.ts
+++ services/server-services/shifts.service.ts
@@ -3,7 +3,7 @@ import { fetchServiceServer } from "./fetch.service.server"
 
 const getUserApplicableShifts = async () => {
     // await fetchServiceServer.GET<Shifts>('applicable_shifts')
-    await fetch('http://localhost:3000/api/applicable_shifts', {method: 'GET'})
+    // await fetch('http://localhost:3000/api/applicable_shifts', {method: 'GET'})
     // path for shifts - /users/<employerID>/applicable_shifts/<employeeId>
 }
 
