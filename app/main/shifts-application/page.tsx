c3cb1ff34c7a2fbbffd3189c6238bc6e95785f05
Shaked Golan <shaked518@gmail.com>
2024-07-20 01:55:18 +0300

    revisioned data structre and workflow txt file

--- app/main/shifts-application/page.tsx
+++ app/main/shifts-application/page.tsx
@@ -1,55 +1,12 @@
 import { shiftService } from "@/services/server-services/shifts.service";
 import ShiftsTable from "./shifts-table";
+import { columns, rows } from "./columns_rows";
 
-// import { rows } from './rows';
-// import { columns } from './columns';
 
 export default async function page() {
    
-    const shiftsTable = await shiftService.getUserApplicableShifts()
-    // console.log(shiftsTable)
-      const rows =  [
-        {
-          key: "1",
-          name: "Tony Reichert",
-          role: "CEO",
-          status: "Active",
-        },
-        {
-          key: "2",
-          name: "Zoey Lang",
-          role: "Technical Lead",
-          status: "Paused",
-        },
-        {
-          key: "3",
-          name: "Jane Fisher",
-          role: "Senior Developer",
-          status: "Active",
-        },
-        {
-          key: "4",
-          name: "William Howard",
-          role: "Community Manager",
-          status: "Vacation",
-        },
-      ];
-
-      const columns = [
-        {
-          key: "name",
-          label: "NAME",
-        },
-        {
-          key: "role",
-          label: "ROLE",
-        },
-        {
-          key: "status",
-          label: "STATUS",
-        },
-      ];
-      
+    // const shiftsTable = await shiftService.getUserApplicableShifts()
+   
  
   return (
     <>
