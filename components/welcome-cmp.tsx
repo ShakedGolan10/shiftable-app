113b03ec2ce91180c38cd1fd172bc8c88f90a304
Shaked Golan <shaked518@gmail.com>
2024-08-11 02:44:58 +0300

    title change

--- components/welcome-cmp.tsx
+++ components/welcome-cmp.tsx
@@ -11,7 +11,7 @@ export default function WelcomeCmp({ onOpen, onLoginDemoUser, isLoadingLogin }) 
             <div className="container mx-auto lg:grid lg:grid-cols-2 lg:gap-x-8">
                 <div className="welcome-page-summery flex flex-col items-start tablet:items-center mb-16 xl:mr-12">
                     <h1 className='text-large mb-5'>Shift management application</h1>
-                    <p className='text-base mb-8'>Start getting things done together with your team based on Shiftable's revolutionary shift management features</p>
+                    <p className='text-base mb-8'>Start getting things done together with your team based on Shiftable's revolutionary shift management features made for small and medium businesses</p>
                     <Button className='login-btn' onClick={onOpen}>Login</Button>
                     <Button color='success' className='hover:bg-transparent hover:text-success-100 border border-success-100' isLoading={isLoadingLogin} onClick={() => onLoginDemoUser(true)}>Try as an Employee demo user</Button>
                     <Button color='warning' className='hover:bg-transparent hover:text-warning-100 border border-warning-100' isLoading={isLoadingLogin} onClick={() => onLoginDemoUser(false)}>Try as an Employer demo user</Button>
