8c9f82fbefa83a47a696abbabbb53615cfc75a65
Shaked Golan <shaked518@gmail.com>
2024-09-04 02:18:56 +0300

    commit for review

--- services/fetch.service.ts
+++ services/fetch.service.ts
@@ -33,6 +33,7 @@ export const fetchService = {
 }
 const api = async (endpoint: string, method: string = 'GET', data: any = null) => {
     const url = (method === 'GET' && data) ? `${BASE_URL}${endpoint}?${createQueryParams(data)}` : `${BASE_URL}${endpoint}`
+    console.log({url})
     try {
         const res = await axios({
             url,
@@ -43,34 +44,7 @@ const api = async (endpoint: string, method: string = 'GET', data: any = null) =
     } catch (err) {
         console.log('arrived error', err)
         console.log(`Had Issues ${method}ing to the backend, endpoint: ${endpoint}, with data: `, data)
-        if (err.response && err.response.status === 401) { // 401 means luck of authentication and that why we clear the session storage and redirect
-            sessionStorage.clear()
-            if (!url.endsWith('/api/user')) window.location.assign('/')
-            return null
-        }
-        throw err
+               throw err
     }
 }
-
-
-async function ajax<T>(endpoint: string, method = 'GET', data = null, query?: object):Promise<T> {
-    try {
-        const res = await axios({
-            url: `${BASE_URL}${endpoint}${((method === 'GET' || method === 'DELETE') && data) ? `/${data}` : ''}`,
-            method,
-            data,
-            params: (method === 'GET' && query) ? query : null
-        })
-        return res.data 
-    } catch (err) {
-        console.log(`Had Issues ${method}ing to the backend, endpoint: ${endpoint}, with data: `, data)
-        console.dir(err)
-        if (err.response && err.response.status === 401) {
-            sessionStorage.clear()
-            window.location.assign('/')
-        }
-        throw err
-    }
-}
-
 
