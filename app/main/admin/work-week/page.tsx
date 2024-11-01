8cc11a66db11a7342f5de56c19ce73ee45a0adf0
Shaked Golan <shaked518@gmail.com>
2024-11-01 19:49:54 +0200

    now fixed

--- app/main/admin/work-week/page.tsx
+++ app/main/admin/work-week/page.tsx
@@ -2,13 +2,13 @@
 import { getWeeklySchedule } from '@/services/server-services/shifts.service';
 import WithDataWrapper from '@/components/helpers/cmp-wrapper';
 import { WorkWeekTable } from './work_week_table';
-import { Employee } from '@/types/class.service';
+import { Employer } from '@/types/class.service';
 import { getThisSunday } from '@/lib/server.utils';
 
 export default function ShiftsApplyPage() {
   const forDate = getThisSunday() 
   const WorkWeekTableWithData = WithDataWrapper({
-    dataPromise: (user: Employee) => getWeeklySchedule(user.employer.id, 'Sun Sep 29 2024'),
+    dataPromise: (user: Employer) => getWeeklySchedule(user.id, 'Sun Sep 29 2024'),
     Component: (props) => <WorkWeekTable {...props} />, 
     errorMsg: 'Couldnt load weekly schedule',
     loadingMsg: 'Loading weekly schedule...'
