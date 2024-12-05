7b8af513b6f74f25ea479771aa31aa88a5cb332a
Shaked Golan <shaked518@gmail.com>
2024-12-05 10:17:02 +0200

    api route established

--- app/api/auth/user/route.ts
+++ app/api/auth/user/route.ts
@@ -10,12 +10,28 @@ interface IUpdateUserPayload {
     userId: string
     name: string
 }
+interface ICreateUserPayload {
+    name: string
+    email: string
+    password: string
+}
 
 export async function PUT(req: IUpdateRequest) {
     try {
           const token = await req.json()
           const {name, userId} = await validateJwtToken<IUpdateUserPayload>(token)
           saveEmployeeName(userId, name)
+        return NextResponse.json('Success', {status: 200})
+    } catch (error) {
+        console.log({error})
+        return new NextResponse(error, { status: 500 })
+    }
+}
+export async function POST(req: IUpdateRequest) {
+    try {
+          const token = await req.json()
+          const {name, email, password} = await validateJwtToken<ICreateUserPayload>(token)
+          // Todo: Create a function that signup a new user and then takes the newId and create a user at db users collection
         return NextResponse.json('Success', {status: 200})
     } catch (error) {
         console.log({error})
