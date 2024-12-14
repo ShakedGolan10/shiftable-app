79ee22dbe830d1df18d0ece1d38ab03cb0001b9e
Shaked Golan <shaked518@gmail.com>
2024-12-15 01:37:44 +0200

    Modified the error page

--- components/helpers/error-element.tsx
+++ components/helpers/error-element.tsx
@@ -1,25 +1,22 @@
-'use client';
-
 import React from 'react';
-import { useRouter } from 'next/navigation';
+import { FaceFrownIcon } from '@heroicons/react/24/outline'; // Importing the icon from @heroicons/react
 import { Button } from '@nextui-org/react';
 
 export default function ErrorElement({ message }: { message: string }) {
-  const router = useRouter();
-
-  const handleBackToMain = () => {
-    router.push('/main');
-  };
-
   return (
-    <div className="flex flex-col items-center justify-center h-screen p-4 bg-red-50">
-      <div className="max-w-md p-6 bg-white shadow-md rounded-lg border border-red-300">
-        <h1 className="text-medium font-bold text-red-600 mb-4 text-center">Error Occurred</h1>
-        <p className="error-msg text-gray-700 text-center mb-6">{message}</p>
-        <Button color="danger" onClick={handleBackToMain} >
-          Go Back to Main Page
-        </Button>
-      </div>
+    <div className="flex flex-col items-center justify-center gap-5 p-4">
+      <h1 className="text-medium font-bold text-red-600 mb-4 text-center">
+        Oops... Something went wrong
+      </h1>
+      <FaceFrownIcon width={100} height={100} />
+      
+      <h1 className="text-subHeader text-center mb-6">
+        We are sorry for the inconvenience. We’re handling this and will return to service shortly.
+      </h1>
+      
+      <Button color="danger" onClick={() => window.location.assign('/main')}>
+        Go Back to Main Page
+      </Button>
     </div>
   );
 }
