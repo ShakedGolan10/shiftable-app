ebae0ef9a3831555b96496433eaf2c92e37b80fa
Shaked Golan <shaked518@gmail.com>
2025-01-16 21:12:18 +0200

    removed unsued imports

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,10 @@ import { LoginForm } from '@/components/login-form'
 import { Navbar, NavbarBrand, useDisclosure } from '@nextui-org/react'
 import WelcomeCmp from '@/components/welcome-cmp'
 import { Employee, Employer } from '@/types/class.service'
-import { useEffect } from 'react'
-import { useRouter } from 'next/navigation'
 
 export default function HomePage() {
-  const { user, login } = useAuth<Employee | Employer>()
+  const { login } = useAuth<Employee | Employer>()
   const {isOpen, onOpen, onClose} = useDisclosure();
-  const router = useRouter()
 
   const demoEmployeeUserCred = {email: process.env.NEXT_PUBLIC_DEMO_USER_EMAIL, password: process.env.NEXT_PUBLIC_DEMO_USER_PASSWORD}
   const demoEmployerUserCred = {email: process.env.NEXT_PUBLIC_DEMO_EMPLOYER_USER_EMAIL, password: process.env.NEXT_PUBLIC_DEMO_EMPLOYER_USER_PASSWORD}
