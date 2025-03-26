5fd261bd3ffb2e85b9a49185bf8c8391fc7697e6
Shaked Golan <shaked518@gmail.com>
2025-03-26 14:32:56 +0200

    for review

--- components/welcome-cmp.tsx
+++ components/welcome-cmp.tsx
@@ -14,11 +14,11 @@ export default function WelcomeCmp({ onOpen, onLoginDemoUser }) {
                 <div className="welcome-page-summery flex flex-col items-start tablet:items-center mb-16 xl:mr-12">
                     <GeneralTitle title={`Shift management application`} />
                     <p className='text-base mb-8'>Start getting things done together with your team based on Shiftable's revolutionary shift management features made for small and medium businesses</p>
-                    <Button color='success' className='hover:bg-transparent text-black border border-warning-100' onClick={onOpen}>Login</Button>
-                    <Button color='primary' className='hover:bg-transparent text-black border border-warning-100' onClick={() => window.location.assign('/signup')}>Onboard as new Employer Now !</Button>
-                    <Button color='secondary' className='hover:bg-transparent text-black border border-success-100' onClick={() => onLoginDemoUser(true)}>Try as an Employee demo user</Button>
-                    <Button color='warning' className='hover:bg-transparent text-black border border-warning-100' onClick={() => onLoginDemoUser(false)}>Try as an Employer demo user</Button>
-                    <a target='_blank' href="https://wa.me/972535302345"><Button color='default' className='hover:bg-transparent text-black border border-warning-100'>Not a member? Contact us!</Button></a>
+                    <Button color='success' className='hover:bg-transparent light:text-black dark:text-white text-black border border-warning-100' onClick={onOpen}>Login</Button>
+                    <Button color='primary' className='hover:bg-transparent light:text-black dark:text-white text-black border border-warning-100' onClick={() => window.location.assign('/signup')}>Onboard as new Employer Now !</Button>
+                    <Button color='secondary' className='hover:bg-transparent light:text-black dark:text-white text-black border border-success-100' onClick={() => onLoginDemoUser(true)}>Try as an Employee demo user</Button>
+                    <Button color='warning' className='hover:bg-transparent light:text-black dark:text-white text-black border border-warning-100' onClick={() => onLoginDemoUser(false)}>Try as an Employer demo user</Button>
+                    <a target='_blank' href="https://wa.me/972535302345"><Button color='default' className='hover:bg-transparent light:text-black dark:text-white text-black border border-warning-100'>Not a member? Contact us!</Button></a>
                 </div>
                 <div className="xl:text-right">
                     <Image className='inline w-auto h-auto' priority src={smartphoneIMG} alt='front page img' width={500} height={500} />
